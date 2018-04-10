056492b30a0b9cce08b6f4fb39fb43c8cd7796ee
Matt Larson <mlarson@usinternet.com>
2018-04-10 07:30:03 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    {{ parcelTest }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      parcelTest: 'Parcel/Vue test'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$primary: #56b983;
+
+.main {
+  color: $primary;
+}
+</style>
+
+
